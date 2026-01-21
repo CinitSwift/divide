8d9ba985903a3720d849431a4f1b902eb19d77c9
言未 <cinitswift@gmail.com>
2026-01-21 19:38:54 +0800

    fix: 临时开启数据库同步以添加新字段

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -28,7 +28,7 @@ import configuration from './config/configuration';
             type: 'postgres',
             url: dbUrl,
             entities: [__dirname + '/**/*.entity{.ts,.js}'],
-            synchronize: configService.get('nodeEnv') !== 'production', // TODO: 表创建完成后改回 configService.get('nodeEnv') !== 'production'
+            synchronize: true, // TODO: 表创建完成后改回 configService.get('nodeEnv') !== 'production'
             ssl: configService.get('database.ssl'),
           };
         }
